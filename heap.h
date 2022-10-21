7e3584255e4653eb4b0f17005f46f27ae2b6a32c
Nicole Concepcion <nconcepc@usc.edu>
2022-10-21 18:35:50 +0000

    Added heap.h

---
+++ heap.h
@@ -0,0 +1,172 @@
+#ifndef HEAP_H
+#define HEAP_H
+#include <functional>
+#include <stdexcept>
+#include <vector>
+#include <algorithm>
+
+
+template <typename T, typename PComparator = std::less<T> >
+class Heap
+{
+public:
+  /**
+   * @brief Construct a new Heap object
+   * 
+   * @param m ary-ness of heap tree (default to 2)
+   * @param c binary predicate function/functor that takes two items
+   *          as an argument and returns a bool if the first argument has
+   *          priority over the second.
+   */
+  Heap(int m=2, PComparator c = PComparator());
+
+  /**
+  * @brief Destroy the Heap object
+  * 
+  */
+  ~Heap();
+
+  /**
+   * @brief Push an item to the heap
+   * 
+   * @param item item to heap
+   */
+  void push(const T& item);
+
+  /**
+   * @brief Returns the top (priority) item
+   * 
+   * @return T const& top priority item
+   * @throw std::underflow_error if the heap is empty
+   */
+  T const & top() const;
+
+  /**
+   * @brief Remove the top priority item
+   * 
+   * @throw std::underflow_error if the heap is empty
+   */
+  void pop();
+
+  /// returns true if the heap is empty
+
+  /**
+   * @brief Returns true if the heap is empty
+   * 
+   */
+  bool empty() const;
+
+    /**
+   * @brief Returns size of the heap
+   * 
+   */
+  size_t size() const;
+
+private:
+  /// Add whatever helper functions and data members you need below
+	int mAry;
+	PComparator comparator;
+	std::vector<T> heapVector;
+};
+
+// Add implementation of member functions here
+
+template <typename T, typename PComparator>
+Heap<T,PComparator>::Heap(int m, PComparator c){
+	 mAry = m;
+	 comparator = c;
+}
+
+template <typename T, typename PComparator>
+Heap<T,PComparator>::~Heap(){
+
+}
+
+template <typename T, typename PComparator>
+void Heap<T,PComparator>::push(const T& item){
+	heapVector.push_back(item);
+	std::size_t index = heapVector.size() - 1;
+  while (index > 0){
+    std::size_t parent_index = (index-1)/mAry;
+    T& child = heapVector[index];
+    T& parent = heapVector[parent_index];
+    if (comparator(parent, child)){
+      break;
+    }
+    std::swap(child, parent);
+    index = parent_index;
+  }
+}
+
+template <typename T, typename PComparator>
+bool Heap<T,PComparator>::empty() const{
+	return heapVector.empty();
+}
+
+template <typename T, typename PComparator>
+size_t Heap<T,PComparator>::size() const{
+	return heapVector.size();
+}
+
+// We will start top() for you to handle the case of 
+// calling top on an empty heap
+template <typename T, typename PComparator>
+T const & Heap<T,PComparator>::top() const
+{
+  // Here we use exceptions to handle the case of trying
+  // to access the top element of an empty heap
+  if(empty()){
+    // ================================
+    // throw the appropriate exception
+    // ================================
+		throw std::underflow_error("Heap is Empty!");
+  }
+  // If we get here we know the heap has at least 1 item
+  // Add code to return the top element
+	return heapVector[0];
+}
+
+
+// We will start pop() for you to handle the case of 
+// calling pop on an empty heap
+template <typename T, typename PComparator>
+void Heap<T,PComparator>::pop()
+{
+  if(empty()){
+    // ================================
+    // throw the appropriate exception
+    // ================================
+		throw std::underflow_error("Heap is Empty!");
+  }
+  std::swap(heapVector[0], heapVector[heapVector.size()-1]);
+  heapVector.pop_back();
+  std::size_t index = 0;
+  while(index < heapVector.size()-1){
+    std::size_t child_index = (mAry*index) + 1;
+    std::size_t cidx = child_index;
+    while (cidx <= (mAry*(index+1))){
+      if (cidx < heapVector.size()){
+        if (comparator(heapVector[cidx], heapVector[child_index])){
+          child_index = cidx;
+        }
+      }
+      cidx++;
+    }
+
+    T& parent = heapVector[index];
+		T& child = heapVector[child_index];
+
+		if(index == heapVector.size() || child_index >= heapVector.size()){
+				break;
+		}
+    if(comparator(parent, child)){
+				break;
+		}
+    std::swap (parent, child);
+    index = child_index;	
+  }
+} 
+
+
+#endif
+
