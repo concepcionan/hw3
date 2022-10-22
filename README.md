0291e91f8f4a9db14cedd78066cabab1956af1a2
Nicole Concepcion <nconcepc@usc.edu>
2022-10-22 06:04:05 +0000

    All Done

---
+++ README.md
@@ -0,0 +1,10 @@
+##HW 3
+
+Nicole Concepcion
+9496883573
+nconcepc@usc.edu
+
+##Notes
+
+To compile and test stack_test,
+g++ -g -Wall stack_test.cpp -I /usr/include/gtest/ -l gtest -l gtest_main -pthread -o stack_test
---
+++ logicsim/event.h
@@ -0,0 +1,25 @@
+#ifndef EVENT_H
+#define EVENT_H
+#include "wire.h"
+
+struct Event
+{
+    uint64_t time;
+    Wire* wire;
+    char state;
+};
+
+typedef struct EventLess {
+    int operator()(Event* a, Event* b) {
+        return a->time < b->time;
+    }
+//write the operator() required to make this a functor that compares Events by time
+} EventLess;
+	
+#endif
+
+// In event.h write the operator() implementation for EventLess to 
+// create a functor for your heap implementation. The operator() will 
+// take two Event*'s as it’s input and needs to sort them using the 
+// Event::time data member in such a way that your heap will be a 
+// min-heap. i.e. Event's are sorted by time.
